1b1e3570dfa4a029e96e847efb45cdc5bdfda46e
Matthew Russell Downey <matthew.r.downey@gmail.com>
2019-04-10 10:50:35 -0400

    Initial commit of Buoyancysort (with errors) covering the basic design/concept/pseudocode

---
+++ Assets/Code/Buoyancysort.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+
+public static class Buoyancysort
+{
+    // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 999, 999, 999, 999, 999, 999, 999, 999, 5
+
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <pseudocode>
+    /// Buoyancysort(array)
+    ///     pivot = Hoare-Partition()
+    ///     if (iteration is even)
+    ///     {
+    ///         // sort left
+    ///         Build-Min-Heap(array[1...pivot-1])
+    ///         for left_iterator = [2...pivot-1]
+    ///             if array[left_iterator - 1] is less than array[left_iterator]
+    ///                 break
+    ///         first_unsorted = 2;
+    ///         if (left_iterator is less than or equal to pivot - 1)
+    ///         {
+    ///             first_known_inversion = left_iterator
+    ///             minimum_inversion = array[first_known_inversion]
+    ///             index_of_minimum_inversion = first_known_inversion
+    ///             for left_iterator = [left_iterator...minimum(2*first_known_inversion, pivot-1)]
+    ///                 if (minimum_inversion is less than array[left_iterator])
+    ///                 {
+    ///                     index_of_minimum_inversion = left_iterator
+    ///                     minimum_inversion = array[left_iterator]
+    ///                 }
+    ///             first_actual_inversion = lower_bound(array, 1, first_known_inversion, minimum_inversion) // e.g. lower_bound() ~= binary_search()
+    ///             swap(array, first_actual_inversion, index_of_minimum_inversion)
+    ///             first_unsorted = first_actual_inversion + 1
+    ///         }
+    ///         else if the whole list was already sorted
+    ///         {
+    ///             first_unsorted = pivot
+    ///         }
+    ///         Buoyancysort(array[first_unsorted...pivot-1]);
+    ///         
+    ///         // sort right
+    ///         Build-Reversed-Max-Heap(array[pivot+1...n])
+    ///         while ()
+    ///         {
+    ///         }
+    ///         Buoyancysort(array[pivot+1...n-1]);
+    ///     }
+    ///     else if (iteration is odd)
+    ///     {
+    ///         Build-Reversed-Max-Heap(array[1...pivot-1])
+    ///         Build-Min-Heap(array[pivot+1...n])
+    ///         //
+    ///         Buoyancysort(array[1...pivot-2]);
+    ///         Buoyancysort(array[pivot+2...n]);
+    ///     }
+    ///     
+    /// </pseudocode>
+    /// <param name="mutable_array"></param>
+    public static sort(ref List<int> mutable_array)
+    {
+    }
+}
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
---
+++ Assets/Code/TestDrivenDevelopment.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDrivenDevelopment : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        int test_cases = 10; //1000;
+        int array_size = 100000; // 10'000'000
+        
+        // Correctness Test
+
+        List<int> sorting_array = new List<int>(array_size);
+        List<int> sorting_array2 = new List<int>(array_size);
+        for (int test_number = 0; test_number < test_cases; test_number += 1)
+        {
+            int control_seed = test_number;
+            for (int array_index = 0; array_index < array_size; array_index += 1)
+            {
+                int random_element = ...
+                sorting_array[array_index] = random_element;
+                sorting_array2[array_index] = random_element;
+            }
+            Buoyancysort.sort(ref sorting_array);
+            sorting_array2.Sort();
+            for (int array_index = 0; array_index < array_size; array_index += 1)
+            {
+                if (sorting_array[array_index] != sorting_array2[array_index])
+                {
+                    // Something is wrong with my sort
+                    Debug.LogError("You fucked up: array[" + array_index + "] = (" + sorting_array[array_index] + ", " + sorting_array2[array_index] + ")");
+                }
+            }
+        }
+
+        // Speed test!
+
+        // Test Buoyancysort
+        sorting_array = new List<int>(array_size);
+        for (int test_number = 0; test_number < test_cases; test_number += 1)
+        {
+            int test_seed = test_number;
+            for (int array_index = 0; array_index < array_size; array_index += 1)
+            {
+                int random_element = ...
+                sorting_array[array_index] = random_element;
+            }
+            // my code - STOPWATCH START
+            Buoyancysort(ref sorting_array);
+            // my code -  STOPWATCH STOP
+        }
+
+        // Test built-in C# sort (if it's a stable algorithm, then this is an unfair comparison).
+        sorting_array = new List<int>(array_size);
+        for (int control_number = 0; control_number < test_cases; control_number += 1)
+        {
+            int control_seed = control_number;
+            for (int array_index = 0; array_index < array_size; array_index += 1)
+            {
+
+            }
+            // my code - STOPWATCH START
+            sorting_array.Sort();
+            // my code -  STOPWATCH STOP
+        }
+    }
+}
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
