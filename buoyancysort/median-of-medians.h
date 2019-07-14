09f69d8c1ba510b490ac2cd91fb103f8c80f5438
Matthew Russell Downey <matthew.r.downey@gmail.com>
2019-07-14 13:23:01 -0400

    Working on median of medians-like algorithm (currently dubbed medianplex)

--- buoyancysort/median-of-medians.h
+++ buoyancysort/median-of-medians.h
@@ -47,7 +47,7 @@ namespace MedianOfMedians
 	}
 
 	/// <summary>
-	/// Mutator - Sorts values in range [first, after_last) using INSERTION-SORT algorithm. No reverse variant necessary.
+	/// Mutator - 
 	/// </summary>
 	/// <param name="Type">The type of data being sorted.</param>
 	/// <param name="tuple_size">The max number of elements sorted (e.g. 5) in each group/tuple.</param>
@@ -55,7 +55,7 @@ namespace MedianOfMedians
 	/// <param name="data">A pointer to a contiguous array of data.</param>
 	/// <param name="first">An index into the <see cref="MedianOfMedians::partition(data)"> representing the element at the start of the range to be partitioned.</param>
 	/// <param name="after_last">An index into the <see cref="MedianOfMedians::partition(data)"> representing the element one-past the end of the range to be partitioned.</param>
-	/// <returns>An index into the <see cref="MedianOfMedians::partition(data)"> representing the "median of medians", which approximates the true median.</returns>
+	/// <returns>An index into the <see cref="MedianOfMedians::partition(data)"> representing an approximation of the "median of medians" (which itself is an approximation of the median).</returns>
 	template <typename Type, int tuple_size, int run_size>
 	std::size_t medianplex(Type *data, std::size_t first, std::size_t after_last)
 	{
@@ -63,11 +63,56 @@ namespace MedianOfMedians
 		// linearly process the data by taking adjacent tuples of size tuple_size with matching sort_group
 		// then sorting them to find a new median of sort_group+1.
 		// HACK:
-		std::vector<Type*> runs = InsertionSort::medians_of_runs<Type, run_size>(data, first, after_last);
-		while (runs.size() > 1)
+
+		// it is best to run this on tuple_size chunks and get the median of those chunks.
+		// This mitigates bad luck (a simple case being merging two medians at the end)
+		std::vector<Type*> upper_medians;
+		for (int tuple_index = 0; tuple_index < tuple_size; tuple_index += 1)
 		{
-			runs = InsertionSort::medians_of_runs<Type, tuple_size>(runs);
+			std::vector<Type*> runs = InsertionSort::medians_of_runs<Type, run_size>(data, first, after_last); // FIXME: you need to take 1/tuple_size of sequence (although this probably isn't the final version anyway)
+			while (runs.size() > 1)
+			{
+				runs = InsertionSort::medians_of_runs<Type, tuple_size>(runs);
+			}
+			upper_medians.push_back(runs[0]);
 		}
-		return runs[0] - data;
+		upper_medians = InsertionSort::medians_of_runs<Type, tuple_size>(upper_medians);
+		return upper_medians[0] - data;
+
+		// Ideally what I want to do is: (for medianplex of 5 -- 3 makes more sense though).
+		// for e.g. 5^3 - 1 (or 124 elements)
+		// <24><25><26><25><24>
+		// <4><5><6><5><4><25><26*><25><4><5><6><5><4>
+		// or e.g. 5^2+1 (or 26 elements)
+		// <5><5><6><5><5>
+		// <5><5><1><1><2><1><1><5><5>
+		// or e.g. 100 elements
+		// <20><20><20><20><20>
+		// <4><4><4><4><4><20><20><20><20> (those 4s are not ideal at all)
+
+		// or e.g. 100 elements (tuple_size = 3) -- I'm sort of curious if I could discern anything from the Collatz_conjecture to help with a derivation of how to split the problem.
+		// <33><34><33>
+		// <<11><11><11>> <<11><12><11>> <<11><11><11>>
+		// <<<3><5><3>> <<3><5><3>> <<3><5><3>>>  <<<3><5><3>> <<3><6><3>> <<3><5><3>>>  <<<3><5><3>> <<3><5><3>> <<3><5><3>>>
+
+		// Ground rules (many of which are different ways of saying the same thing):
+		// For even numbers, put an even number in the center (avoid bias)
+		// Always make middle number greater than left and right (avoid lopsidedness) -- is this really a good idea?
+		// Always make left and right equal (avoid bias)
+		// Always ensure left (or right) and center differ by at most ? (3?)
+		// The only number that can be even is the center (left and right are always odd)
+
+		// Another idea: what are the ways you combine? -- even numbers always seem to discard some information being considered (is that necessary? -- I think it is)
+		// 1 -> <1>
+		// 3 -> <1><1><1>
+		// 5 -> <1><3><1> // special case <1>{5}
+		// 7 -> <3><1><3> // special case <1>{7}
+		// 9 -> <3><3><3> // special case <1>{9}
+		// 11 -> <3><5><3> // special case <1>{11}
+		// 13 -> <5><3><5> // special case <1>{13}
+		// 15 -> <5><5><5>
+		// 17 -> <5><7><5> -- you can basically continue this forever. If you get an even number you should remove one value for simplicity.
+
+		// I'm still trying to figure out if any of this is useful...
 	}
 }
