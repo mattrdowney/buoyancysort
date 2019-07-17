854d1b34093967e6a5b7e794a736adb0bb4473ab
Matthew Russell Downey <matthew.r.downey@gmail.com>
2019-07-16 21:23:29 -0400

    Fixed the easy bugs in InterlacedBuildDoubleBinaryHeap

--- buoyancysort/interlaced-double-binary-heap.h
+++ buoyancysort/interlaced-double-binary-heap.h
@@ -12,8 +12,8 @@ namespace InterlacedDoubleBinaryHeap
 		// Base case is build_index == first (no need to swap an element with itself and you need to stop the recursion).
 		if (build_index > first)
 		{
-			std::size_t forward_build_index = build_index;
-			std::size_t reverse_build_index = (after_last - 1) - (build_index - first);
+			std::size_t forward_build_index = (after_last - 1) - build_index;
+			std::size_t reverse_build_index = first + build_index;
 			std::size_t forward_parent_index = Heap::parent(forward_build_index, after_last - 1);
 			std::size_t reverse_parent_index = Heap::parent(reverse_build_index, first);
 			if ((build_index-first) % 2 == 0)
@@ -31,8 +31,8 @@ namespace InterlacedDoubleBinaryHeap
 				reverse_build_index -= 1;
 				reverse_parent_index -= 1;
 			}
-			std::size_t lower_power = PowerOfTwo::lower_power_of_two(forward_build_index-first)+first;
-			while (forward_build_index > lower_power)
+			std::size_t lower_power = PowerOfTwo::lower_power_of_two(build_index-first)+first;
+			while (reverse_build_index >= lower_power)
 			{
 				if (data[forward_build_index] > data[forward_parent_index])
 				{
@@ -70,11 +70,10 @@ namespace InterlacedDoubleBinaryHeap
 	void build(Type *data, std::size_t first, std::size_t after_last) // FIXME: buoyancysort is broken until this maintains both heap properties
 	{
 		std::size_t build_index = after_last - 1;
-		std::size_t lower_power;
 		while (build_index > first)
 		{
 			build_strip<Type>(data, first, after_last, build_index);
-			lower_power = PowerOfTwo::lower_power_of_two(build_index - first);
+			std::size_t lower_power = PowerOfTwo::lower_power_of_two(build_index - first);
 			if (build_index == lower_power + first)
 			{
 				// E.g. lower_power(256) == 256, but we actually want 128 
