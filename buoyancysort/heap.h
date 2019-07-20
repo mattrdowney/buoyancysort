eaa72f2ebd0e8121a8f7f5eb78db6e39c28a473d
Matthew Russell Downey <matthew.r.downey@gmail.com>
2019-07-20 18:06:57 -0400

    Working on a correct min_heapify() and max_heapify() algorithm -- both super buggy

--- buoyancysort/heap.h
+++ buoyancysort/heap.h
@@ -6,24 +6,28 @@
 namespace Heap
 {
 	/// -1/0/+1 - based
-	long long parent(long long child, long long center)
+	template <typename Type>
+	Type parent(Type child, Type center)
 	{
 		return (child - center) / 2 + center;
 	}
 
 	/// -1/0/+1 - based
-	long long left_child(long long parent, long long center)
+	template <typename Type>
+	Type left_child(Type parent, Type center)
 	{
 		return (parent - center) * 2 - (parent <= center) + center;
 	}
 
 	/// -1/0/+1 - based
-	long long right_child(long long parent, long long center)
+	template <typename Type>
+	Type right_child(Type parent, Type center)
 	{
 		return (parent - center) * 2 + (parent >= center) + center; 
 	}
 
-	long long sibling(long long child, long long center)
+	template <typename Type>
+	Type sibling(Type child, Type center)
 	{
 		// I can't think of how to remove branch prediction (the compiler is optimizing my code, but it doesn't seem to remove comparisons entirely)
 		if (center - child > 1)
@@ -38,147 +42,75 @@ namespace Heap
 	}
 
 	template <typename Type>
-	void build_max_strip(Type *data, std::size_t first, std::size_t after_last, std::size_t build_index) // FIXME: buoyancysort is broken until this maintains both heap properties
+	std::size_t min_heapify(Type *data, std::size_t first, std::size_t after_last, std::size_t root)
 	{
-		// Base case is build_index == first (no need to swap an element with itself and you need to stop the recursion).
-		if (build_index > first + 1)
+		std::size_t left = Heap::left_child(root, first);
+		if (left < after_last) // OPTIMIZATION: early return
 		{
-			std::size_t forward_build_index = (after_last - 1) - build_index;
-			std::size_t forward_parent_index = Heap::parent(forward_build_index, after_last - 1);
-			if ((build_index - first) % 2 == 0)
+			std::size_t smallest = root;
+			if (data[smallest] > data[left]) // OPTIMIZATION: left is smaller than (or equal to) right in sorted order
 			{
-				if (data[forward_build_index] > data[forward_parent_index])
-				{
-					std::swap(data[forward_build_index], data[forward_parent_index]);
-				}
-				forward_build_index += 1;
-				forward_parent_index += 1;
+				smallest = left;
 			}
-			std::size_t lower_power = PowerOfTwo::lower_power_of_two(build_index - first) + first;
-			while (reverse_build_index >= lower_power)
+			std::size_t right = left + 1; // OPTIMIZATION: only works because first is the pivot
+			if (right < after_last && data[smallest] > data[right])
 			{
-				if (data[forward_build_index] > data[forward_build_index + 1])
-				{
-					std::swap(data[forward_build_index], data[forward_build_index + 1]);
-				}
-				if (data[forward_build_index + 1] > data[forward_parent_index])
-				{
-					std::swap(data[forward_build_index + 1], data[forward_parent_index]);
-				}
+				smallest = right;
+			}
+			if (smallest != root)
+			{
+				std::swap(data[root], data[smallest]);
+				return max_heapify(data, first, after_last, smallest); // NOTE: "smallest" has since changed.
+			}
+		}
+		return root;
+	}
 
-				forward_build_index += 2;
-				forward_parent_index += 1;
-			}
-			// This is often of the form 2^i-1 (where i is an integer).
-			std::size_t build_index_parent = Heap::parent(build_index, first);
-			// Recursively build strips e.g. 128, 64, 32, 16, 8, 4, 2, 1, 0
-			// While it's not immediately apparent, cache coherency is very good.
-			// Strips are essentially proccessed linearly to the root of the heap.
-			build_max_strip<Type>(data, first, after_last, build_index_parent);
-		}
-		else if (build_index == first + 1)
+	// TODO: VERIFY: does this sort of std::ptrdiff_t usage result in undefined behavior? 
+	template <typename Type>
+	std::ptrdiff_t max_heapify(Type *data, std::ptrdiff_t first, std::ptrdiff_t after_last, std::ptrdiff_t root)
+	{
+		std::ptrdiff_t right = Heap::right_child(root, after_last-1); // NOTE: std::ptrdiff_t (or signed type) required or underflow might occur
+		if (first <= right) // OPTIMIZATION: early return
 		{
-			if (data[after_last - 2] > data[after_last - 1])
+			std::ptrdiff_t largest = root;
+			if (data[right] > data[largest]) // OPTIMIZATION: right is larger than (or equal to) left in sorted order
 			{
-				std::swap(data[after_last - 2], data[after_last - 1]);
+				largest = right;
+			}
+			std::ptrdiff_t left = right - 1; // OPTIMIZATION: only works because after_last-1 is the pivot
+			if (first <= left && data[left] > data[largest])
+			{
+				largest = left;
+			}
+			if (largest != root)
+			{
+				std::swap(data[root], data[largest]);
+				return max_heapify(data, first, after_last, largest); // NOTE: "largest" has since changed.
 			}
 		}
+		return root;
 	}
 
 	template <typename Type>
 	void build_max_heap(Type *data, std::size_t first, std::size_t after_last) // FIXME: buoyancysort is broken until this maintains both heap properties
 	{
-		std::size_t build_index = after_last - 1;
-		while (build_index > first)
+		std::size_t max_heapify_from = Heap::parent(first, after_last-1);
+		while (max_heapify_from < after_last-1)
 		{
-			build_max_strip<Type>(data, first, after_last, build_index);
-			std::size_t lower_power = PowerOfTwo::lower_power_of_two(build_index - first);
-			if (build_index == lower_power + first)
-			{
-				// E.g. lower_power(256) == 256, but we actually want 128 
-				build_index = lower_power / 2 + first;
-			}
-			else
-			{
-				build_index = lower_power + first;
-			}
-			for (int i = first; i < after_last; i += 1)
-			{
-				std::cout << data[i] << " ";
-			}
-			std::cout << std::endl << std::endl;
-		}
-	}
-
-	template <typename Type>
-	void build_min_strip(Type *data, std::size_t first, std::size_t after_last, std::size_t build_index) // FIXME: buoyancysort is broken until this maintains both heap properties
-	{
-		// Base case is build_index == first (no need to swap an element with itself and you need to stop the recursion).
-		if (build_index > first + 1)
-		{
-			std::size_t reverse_build_index = first + build_index;
-			std::size_t reverse_parent_index = Heap::parent(reverse_build_index, first);
-			if ((build_index - first) % 2 == 0)
-			{
-				if (data[reverse_parent_index] > data[reverse_build_index])
-				{
-					std::swap(data[reverse_parent_index], data[reverse_build_index]);
-				}
-				reverse_build_index -= 1;
-				reverse_parent_index -= 1;
-			}
-			std::size_t lower_power = PowerOfTwo::lower_power_of_two(build_index - first) + first;
-			while (reverse_build_index >= lower_power)
-			{
-				if (data[reverse_build_index - 1] > data[reverse_build_index])
-				{
-					std::swap(data[reverse_build_index - 1], data[reverse_build_index]);
-				}
-				if (data[reverse_parent_index] > data[reverse_build_index - 1])
-				{
-					std::swap(data[reverse_parent_index], data[reverse_build_index - 1]);
-				}
-				reverse_build_index -= 2;
-				reverse_parent_index -= 1;
-			}
-			// This is often of the form 2^i-1 (where i is an integer).
-			std::size_t build_index_parent = Heap::parent(build_index, first);
-			// Recursively build strips e.g. 128, 64, 32, 16, 8, 4, 2, 1, 0
-			// While it's not immediately apparent, cache coherency is very good.
-			// Strips are essentially proccessed linearly to the root of the heap.
-			build_min_strip<Type>(data, first, after_last, build_index_parent);
-		}
-		else if (build_index == first + 1)
-		{
-			if (data[first] > data[first + 1])
-			{
-				std::swap(data[first], data[first + 1]);
-			}
+			max_heapify(data, first, after_last, max_heapify_from);
+			max_heapify_from += 1;
 		}
 	}
 
 	template <typename Type>
 	void build_min_heap(Type *data, std::size_t first, std::size_t after_last) // FIXME: buoyancysort is broken until this maintains both heap properties
 	{
-		std::size_t build_index = after_last - 1;
-		while (build_index > first)
+		std::size_t min_heapify_from = Heap::parent(after_last - 1, first);
+		while (min_heapify_from > first)
 		{
-			build_min_strip<Type>(data, first, after_last, build_index);
-			std::size_t lower_power = PowerOfTwo::lower_power_of_two(build_index - first);
-			if (build_index == lower_power + first)
-			{
-				// E.g. lower_power(256) == 256, but we actually want 128 
-				build_index = lower_power / 2 + first;
-			}
-			else
-			{
-				build_index = lower_power + first;
-			}
-			for (int i = first; i < after_last; i += 1)
-			{
-				std::cout << data[i] << " ";
-			}
-			std::cout << std::endl << std::endl;
+			min_heapify(data, first, after_last, min_heapify_from);
+			min_heapify_from -= 1;
 		}
 	}
 }
--- buoyancysort/interlaced-double-binary-heap.h
+++ buoyancysort/interlaced-double-binary-heap.h
@@ -7,107 +7,24 @@
 namespace InterlacedDoubleBinaryHeap
 {
 	template <typename Type>
-	void build_strip(Type *data, std::size_t first, std::size_t after_last, std::size_t build_index) // FIXME: buoyancysort is broken until this maintains both heap properties
-	{
-		// Base case is build_index == first (no need to swap an element with itself and you need to stop the recursion).
-		if (build_index > first + 1)
-		{
-			std::size_t forward_build_index = (after_last - 1) - build_index;
-			std::size_t reverse_build_index = first + build_index;
-			std::size_t forward_parent_index = Heap::parent(forward_build_index, after_last - 1);
-			std::size_t reverse_parent_index = Heap::parent(reverse_build_index, first);
-			if ((build_index-first) % 2 == 0)
-			{
-				if (data[forward_build_index] > data[forward_parent_index])
-				{
-					std::swap(data[forward_build_index], data[forward_parent_index]);
-				}
-				if (data[reverse_parent_index] > data[reverse_build_index])
-				{
-					std::swap(data[reverse_parent_index], data[reverse_build_index]);
-				}
-				forward_build_index += 1;
-				forward_parent_index += 1;
-				reverse_build_index -= 1;
-				reverse_parent_index -= 1;
-			}
-			std::size_t lower_power = PowerOfTwo::lower_power_of_two(build_index-first)+first;
-			while (reverse_build_index >= lower_power)
-			{
-				if (data[forward_build_index] > data[forward_build_index+1])
-				{
-					std::swap(data[forward_build_index], data[forward_build_index+1]);
-				}
-				if (data[forward_build_index+1] > data[forward_parent_index])
-				{
-					std::swap(data[forward_build_index+1], data[forward_parent_index]);
-				}
-
-				if (data[reverse_build_index-1] > data[reverse_build_index])
-				{
-					std::swap(data[reverse_build_index - 1], data[reverse_build_index]);
-				}
-				if (data[reverse_parent_index] > data[reverse_build_index-1])
-				{
-					std::swap(data[reverse_parent_index], data[reverse_build_index-1]);
-				}
-
-				forward_build_index += 2;
-				forward_parent_index += 1;
-				reverse_build_index -= 2;
-				reverse_parent_index -= 1;
-			}
-			// This is often of the form 2^i-1 (where i is an integer).
-			std::size_t build_index_parent = Heap::parent(build_index, first);
-			// Recursively build strips e.g. 128, 64, 32, 16, 8, 4, 2, 1, 0
-			// While it's not immediately apparent, cache coherency is very good.
-			// Strips are essentially proccessed linearly to the root of the heap.
-			build_strip<Type>(data, first, after_last, build_index_parent);
-		}
-		else if (build_index == first+1)
-		{
-			if (data[first] > data[first+1])
-			{
-				std::swap(data[first], data[first+1]);
-			}
-			if (data[after_last-2] > data[after_last-1])
-			{
-				std::swap(data[after_last-2], data[after_last-1]);
-			}
-		}
-	}
-
-	template <typename Type>
 	void build(Type *data, std::size_t first, std::size_t after_last) // FIXME: buoyancysort is broken until this maintains both heap properties
 	{
-		std::size_t build_index = after_last - 1;
-		while (build_index > first)
+		std::size_t max_heapify_from = Heap::parent(first, after_last - 1);
+		std::size_t min_heapify_from = Heap::parent(after_last - 1, first);
+		while (max_heapify_from < after_last - 1)
 		{
-			build_strip<Type>(data, first, after_last, build_index);
-			std::size_t lower_power = PowerOfTwo::lower_power_of_two(build_index - first);
-			if (build_index == lower_power + first)
-			{
-				// E.g. lower_power(256) == 256, but we actually want 128 
-				build_index = lower_power/2 + first;
-			}
-			else
-			{
-				build_index = lower_power + first;
-			}
-			for (int i = first; i < after_last; i += 1)
-			{
-				std::cout << data[i] << " ";
-			}
-			std::cout << std::endl << std::endl;
+			max_heapify(data, first, after_last, max_heapify_from);
+			min_heapify(data, first, after_last, min_heapify_from);
+			max_heapify_from += 1;
+			min_heapify_from -= 1;
 		}
 	}
 
-	// TODO: RESEARCH: keep heap structure on both sides.
-	//     alternate on build-min-heap and build-max-heap calls (using the recursive variant, not iterative)
-	//     when a element must be swapped, get the complementary_node using (approximately) `1 - n`
-	//     if (complementary_node is meaningful i.e. it is not on the same side as gibberish values according to a partition)
-	//         if (complementary_node invalidates it's parent)
-	//             "sift-up"
-	//         else if (complementary_node invalidates it's children)
-	//             "sift-down"
+	// use min_heapify() and max_heapify() in an alternating fasion (as subroutines).
+	// when the return isn't the index, swaps were made and you are given a descendent child.
+	// Find the list of decendents all the way up until the ancestor.
+	// Add that list of decendents to a std::ordered_set<std::size_t> of dubious_nodes
+	// At least when those nodes are past the "lines of trust".
+	// TODO: RESEARCH: how do "lines of trust" work? When are they invalidated? When are they still valid/"trusty"?
+	// NOTE: "dubious" and "trusty" are references to Dijkstra's smoothsort.
 }
