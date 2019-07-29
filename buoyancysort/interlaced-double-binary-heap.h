57c4714a0f00247d3550263901577b85d4ebbec9
Matthew Russell Downey <matthew.r.downey@gmail.com>
2019-07-28 22:40:00 -0400

    Working on InterlacedDoubleBinaryHeap::rebuild() data structures (perfect heap) -- theoryland

--- buoyancysort/interlaced-double-binary-heap.h
+++ buoyancysort/interlaced-double-binary-heap.h
@@ -95,15 +95,15 @@ namespace InterlacedDoubleBinaryHeap
 	enum class AdjustType { Undecided, Min, Max };
 
 	template <typename Type>
-	void verify(Type *data, long before_first, long after_last, std::set<long> &dubious_min_nodes, std::set<long> &dubious_max_nodes)
+	void rebuild(Type *data, long before_first, long after_last, std::set<long> &dubious_min_nodes, std::set<long> &dubious_max_nodes)
 	{
 		Print::print(data, before_first, after_last);
 
 		enhance_dubious_min_heap_elements(data, before_first, after_last, dubious_min_nodes);
 		enhance_dubious_max_heap_elements(data, before_first, after_last, dubious_max_nodes);
 
-		std::set<long> next_dubious_min_nodes;
-		std::set<long> next_dubious_max_nodes;
+		std::set<long> next_dubious_min_nodes{ before_first + 1 }; // sentinel nodes, which simplify logic (in my opinion)
+		std::set<long> next_dubious_max_nodes{ after_last - 1 };
 
 		std::set<long>::reverse_iterator min_iterator = dubious_min_nodes.rbegin();
 		std::set<long>::iterator max_iterator = dubious_max_nodes.begin();
@@ -165,15 +165,28 @@ namespace InterlacedDoubleBinaryHeap
 
 		if (next_dubious_min_nodes.size() > 0 || next_dubious_max_nodes.size() > 0)
 		{
-			verify(data, before_first, after_last, next_dubious_min_nodes, next_dubious_max_nodes);
+			rebuild(data, before_first, after_last, next_dubious_min_nodes, next_dubious_max_nodes);
 		}
 	}
 
 	template <typename Type>
 	void build(Type *data, long before_first, long after_last)
 	{
-		std::set<long> dubious_min_nodes; // essentially a std::ordered_set<long> (not a std::unordered_set<long>)
-		std::set<long> dubious_max_nodes;
+		// FIXME: The problem here: if I add e.g. ~n/2 nodes to this ordered_set (e.g. binary tree) then the performance devolves to O(nlgn)...
+		// I could use a std::unordered_set<long>, but I don't know if even perfect hashing here would guarantee constant-time access.
+		// There's definitely a good data structure I just need to find it
+		// (prioritizing constant-time insertion (with duplicates), never really deleting, and retrieving an inorder traversal once -- all in O(n) time)
+		// Alternatively, I could go for the whole build *times* as_many_times_as_i_need_to_get_the_job_done, with a small optimization where you can get the minimum / maximum "trusty" indices.
+		// I basically want a sparse array representation, so maybe I should see if any of those (e.g. Skip List) could work better.
+		// Most likely anything I try devolves to O(lgn) for insert key, even if traversing the inorder list is quick.
+		// a van Emde Boas tree would probably be a step in the right direction, but I honestly think perfect hashing might be the only solution with any real hope upon thinking about this more.
+		// (upon further research) Perfect hashing seems to work here, although getting an inorder traversal is difficult since you have to do a build heap call as a subroutine (absurd, right?)
+		// just to get a rough priority queue (and you couldn't actually dequeue without incurring O(nlgn)).
+		// At the same time, I guess you don't need the "lines of trust" concept anymore, you could just check if the node is "dubious"/"trusty".
+		// I think you can do this with perfect hashing, where the size is (after_last - (before_first + 1)), so that will be how I approach this from now on.
+		// I will implement the code with std::unordered_set<long> now, but keep in mind that a perfect hash performs better in the worst case.
+		std::set<long> dubious_min_nodes{ before_first + 1 }; // essentially a std::ordered_set<long> (not a std::unordered_set<long>)
+		std::set<long> dubious_max_nodes{ after_last - 1 }; // sentinel nodes, which simplify logic (in my opinion)
 		long dubious_node;
 		long max_right_line_of_trust = MaxHeap::parent(before_first + 1, after_last); // when on a line of trust, a node is still trusted
 		long max_left_line_of_trust = max_right_line_of_trust - 1;
@@ -201,7 +214,7 @@ namespace InterlacedDoubleBinaryHeap
 		}
 		std::cout << std::endl;
 
-		verify(data, before_first, after_last, dubious_min_nodes, dubious_max_nodes);
+		rebuild(data, before_first, after_last, dubious_min_nodes, dubious_max_nodes);
 	}
 
 	// use min_heapify() and max_heapify() in an alternating fasion (as subroutines).
