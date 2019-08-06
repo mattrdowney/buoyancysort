83e93086a3d24454ded386ec61f5c74b54b21f65
Matthew Russell Downey <matthew.r.downey@gmail.com>
2019-08-06 14:02:50 -0400

    InterlacedEntangledDoubleBinaryHeap::build() extra outlining with reference to sort_min/max_siblings()

--- buoyancysort/interlaced-entangled-double-binary-heap.h
+++ buoyancysort/interlaced-entangled-double-binary-heap.h
@@ -8,10 +8,11 @@
 // NOTE: "dubious" and "trusty" are references to Dijkstra's smoothsort.
 namespace InterlacedEntangledDoubleBinaryHeap // even if this doesn't work, it's still interesting to develop
 {
-	/// STATUS: believed to be final. No changes pending.
+	/// STATUS: 100% believed to be final and not a copy-paste error. -9999 changes pending.
 	template <typename Type>
 	void build(Type *data, long before_first, long after_last)
 	{
+		// TODO: Have I created a memory leak anywhere? I forgot I should be checking for that.
 		std::vector<bool> trusty_matrix;
 		std::vector<char> depth_matrix;
 		std::vector<std::vector<long>> dubious_min_nodes;
@@ -28,7 +29,7 @@ namespace InterlacedEntangledDoubleBinaryHeap // even if this doesn't work, it's
 				min_right_line_of_implicit_trust, max_left_line_of_implicit_trust);
 
 		std::vector<std::vector<long>> dubious_min_siblings(dubious_min_nodes); // Attempting deep copy, which is important because this...
-		std::vector<std::vector<long>> dubious_max_siblings(dubious_max_nodes); // ... is a vector<vector>(the inner vectors are .clear()'ed).
+		std::vector<std::vector<long>> dubious_max_siblings(dubious_max_nodes); // ... is a vector<vector>(the inner vectors are .clear()'ed)
 
 		while (true)
 		{
@@ -39,9 +40,50 @@ namespace InterlacedEntangledDoubleBinaryHeap // even if this doesn't work, it's
 					min_right_line_of_implicit_trust, max_left_line_of_implicit_trust);
 
 			bool change_attempted = false;
+			for (long depth = dubious_min_siblings.size() - 1; depth >= 0; depth -= 1)
+			{
+				if (dubious_min_siblings[depth].size() > 0 || dubious_max_siblings[depth].size() > 0)
+				{
+					change_attempted = true;
+					int min_iterator = dubious_min_siblings[depth].size();
+					int max_iterator = dubious_max_siblings[depth].size();
+					while (min_iterator < max_iterator)
+					{
+						max_iterator -= 1;
+						//sort_max_siblings()
+					}
+					while (min_iterator > max_iterator)
+					{
+						min_iterator -= 1;
+						//sort_min_siblings()
+					}
+					while (min_iterator > 0)
+					{
+						min_iterator -= 1;
+						max_iterator -= 1;
+						//sort_min_siblings()
+						//sort_max_siblings()
+					}
+					dubious_min_siblings[depth].clear();
+					dubious_max_siblings[depth].clear();
+				}
+			}
 
 			// compare/sort siblings  (invalidating both sides of heap where necessary)
 			// remember to update: 1) trusty_matrix (both sides), 2) dubious_min/max_nodes, 3) [at the end] dubious_min/max_siblings
+
+			// copy dubious_min/max_nodes into dubious_min/max_siblings (attempt to avoid a memory leak)
+			for (long depth = 0; depth < dubious_min_siblings.size(); depth += 1)
+			{
+				for (long min_iterator = 0; min_iterator < dubious_min_nodes[depth].size(); min_iterator += 1)
+				{
+					dubious_min_siblings.push_back(dubious_min_nodes[depth][min_iterator]);
+				}
+				for (long max_iterator = 0; max_iterator < dubious_max_nodes[depth].size(); max_iterator += 1)
+				{
+					dubious_max_siblings.push_back(dubious_max_nodes[depth][max_iterator]);
+				}
+			}
 
 			if (!change_attempted)
 			{
