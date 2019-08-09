984918c2d991d9d368379bca370806e501631029
Matthew Russell Downey <matthew.r.downey@gmail.com>
2019-08-09 16:46:52 -0400

    Reverted to old version of Sift::leftward/rightward_sift() and fixed bug

--- buoyancysort/sift.h
+++ buoyancysort/sift.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include "insertion-sort.h"
 #include "reverse.h"
 
 namespace Sift
@@ -13,30 +14,44 @@ namespace Sift
 	/// <param name="before_first">An index into the <see cref="InsertionSort::sort(data)"> representing the element one-before the start of the range to be sifted.</param>
 	/// <param name="after_last">An index into the <see cref="InsertionSort::sort(data)"> representing the element one-past the end of the range to be sifted.</param>
 	template <typename Type>
-	void leftward_sift(Type *data, long before_first, long after_last) // Notably, this version of sift could be "worse", so I should keep that in mind. (e.g. 9 8 7 6 4 3 2 1 5 would have been sorted by the old algorithm but not the new one)
+	void leftward_sift(Type *data, long before_first, long after_last)
 	{
 		long minimum_cursor = after_last - 1;
 		while (before_first + 1 < minimum_cursor)
 		{
-			long sequence_cursor = minimum_cursor - 1;
-			if (data[sequence_cursor] > data[sequence_cursor + 1])
+			if (before_first + 2 < minimum_cursor) // at least two elements remaining
 			{
-				// decreasing sequence that needs to be reversed
-				while (before_first + 1 < sequence_cursor && data[sequence_cursor - 1] > data[sequence_cursor])
+				long sequence_cursor = minimum_cursor - 2;
+				if (data[sequence_cursor] >= data[sequence_cursor + 1]) // NOTE: I went for the unstable version XD
 				{
-					sequence_cursor -= 1;
+					// decreasing (more accurately: non-increasing) sequence that needs to be reversed
+					while (before_first + 1 < sequence_cursor && data[sequence_cursor - 1] >= data[sequence_cursor])
+					{
+						sequence_cursor -= 1;
+					}
+					Reverse::reverse(data, sequence_cursor, minimum_cursor);
+					// You reverse the minimum_cursor element, so you have to insert it from the opposite side
+					InsertionSort::insert_from_left(data, sequence_cursor, minimum_cursor + 1);
 				}
-				Reverse::reverse(data, sequence_cursor, minimum_cursor);
+				else // <
+				{
+					// increasing sequence that can be left alone
+					while (before_first + 1 < sequence_cursor && data[sequence_cursor - 1] < data[sequence_cursor])
+					{
+						sequence_cursor -= 1;
+					}
+					InsertionSort::insert_from_right(data, sequence_cursor - 1, minimum_cursor);
+				}
+				minimum_cursor = sequence_cursor;
 			}
-			else // <=
+			else // only one remaining element that hasn't been compared to minimum // TODO: I think it would be simpler if I removed this case and recompared minimum_cursor
 			{
-				// increasing sequence that can be left alone
-				while (before_first + 1 < sequence_cursor && data[sequence_cursor - 1] <= data[sequence_cursor])
+				if (data[before_first + 1] > data[minimum_cursor])
 				{
-					sequence_cursor -= 1;
+					std::swap(data[before_first + 1], data[minimum_cursor]);
 				}
+				minimum_cursor -= 1;
 			}
-			minimum_cursor = sequence_cursor;
 		}
 	}
 
@@ -48,30 +63,44 @@ namespace Sift
 	/// <param name="before_first">An index into the <see cref="InsertionSort::sort(data)"> representing the element one-before the start of the range to be sifted.</param>
 	/// <param name="after_last">An index into the <see cref="InsertionSort::sort(data)"> representing the element one-past the end of the range to be sifted.</param>
 	template <typename Type>
-	void rightward_sift(Type *data, long before_first, long after_last) // Notably, this version of sift could be "worse", so I should keep that in mind.
+	void rightward_sift(Type *data, long before_first, long after_last)
 	{
 		long maximum_cursor = before_first + 1;
 		while (maximum_cursor < after_last - 1)
 		{
-			long sequence_cursor = maximum_cursor + 1;
-			if (data[sequence_cursor - 1] > data[sequence_cursor])
+			if (maximum_cursor < after_last - 2) // at least two elements remaining
 			{
-				// decreasing (more accurately: non-increasing) sequence that needs to be reversed
-				while (sequence_cursor < after_last - 1 && data[sequence_cursor] > data[sequence_cursor + 1])
+				long sequence_cursor = maximum_cursor + 2;
+				if (data[sequence_cursor - 1] >= data[sequence_cursor]) // NOTE: I went for the unstable version XD -- I almost feel like it's more inefficient but I'll keep it for now
 				{
-					sequence_cursor += 1;
+					// decreasing (more accurately: non-increasing) sequence that needs to be reversed
+					while (sequence_cursor < after_last - 1 && data[sequence_cursor] >= data[sequence_cursor + 1])
+					{
+						sequence_cursor += 1;
+					}
+					Reverse::reverse(data, maximum_cursor, sequence_cursor);
+					// You reverse the minimum_cursor element, so you have to insert it from the opposite side
+					InsertionSort::insert_from_right(data, maximum_cursor - 1, sequence_cursor);
 				}
-				Reverse::reverse(data, maximum_cursor, sequence_cursor);
+				else // <
+				{
+					// increasing sequence that can be left alone
+					while (sequence_cursor < after_last - 1 && data[sequence_cursor] < data[sequence_cursor + 1])
+					{
+						sequence_cursor += 1;
+					}
+					InsertionSort::insert_from_left(data, maximum_cursor, sequence_cursor + 1);
+				}
+				maximum_cursor = sequence_cursor;
 			}
-			else // <=
+			else // only one remaining element that hasn't been compared to maximum
 			{
-				// increasing sequence that can be left alone
-				while (sequence_cursor < after_last - 1 && data[sequence_cursor] <= data[sequence_cursor + 1])
+				if (data[maximum_cursor] > data[after_last - 1])
 				{
-					sequence_cursor += 1;
+					std::swap(data[maximum_cursor], data[after_last - 1]);
 				}
+				maximum_cursor += 1;
 			}
-			maximum_cursor = sequence_cursor;
 		}
 	}
 }
