57b7e3297c578f8a9cbe6f59c72f7dd6a6dd7e43
Matthew Russell Downey <matthew.r.downey@gmail.com>
2019-08-25 08:42:57 -0400

    Ordered Min Heap compiles and runs but doesn't work properly

--- buoyancysort/main.cpp
+++ buoyancysort/main.cpp
@@ -14,6 +14,8 @@
 #include "max-heap.h"
 #include "median-of-medians.h"
 #include "min-heap.h"
+#include "ordered-max-heap.h"
+#include "ordered-min-heap.h"
 #include "print.h"
 #include "reverse.h"
 #include "shell-sort.h"
@@ -26,7 +28,7 @@ typedef IntThatTracksComparisons::IntThatTracksComparisons current_type;
 int main()
 {
 	HeapTests::heap_tests();
-	const std::size_t size = 30;
+	const std::size_t size = 100;
 	current_type data[size]; // TODO: define IntThatTracksComparisons that overrides definitions of < >, <=, >=, ==, != and increments a static counter when they are called
 	// the cool thing about the IntThatTracksComparisons function is it can work with std::partition, TimSort, etc (even if it has a blackbox implementation) -- plus it's easier to implement
 	for (std::size_t i = 0; i < size; i += 1)
@@ -48,7 +50,7 @@ int main()
 
 	IntThatTracksComparisons::reset_comparisons();
 
-	MinHeap::build<current_type>((current_type*)data, -1, size, 5);
+	OrderedMinHeap::build<current_type>((current_type*)data, -1, size, 5);
 
 	std::size_t comparisons = IntThatTracksComparisons::get_comparisons();
 
--- buoyancysort/ordered-min-heap.h
+++ buoyancysort/ordered-min-heap.h
@@ -22,8 +22,8 @@ namespace OrderedMinHeap
 		depth_matrix.resize(bitset_size); // NOTE: Theoretically, this *could* fail with certain char/long definitions (sort of undefined behavior)
 
 		long array_index = 0;
-		long memset_index_begin = 1;
-		long memset_index_end = memset_index_begin + 1;
+		long memset_index_begin = 1; // FIXME: This isn't quite right...
+		long memset_index_end = tuple_size;
 
 		// Because of the nature of next_power_of_k() (time complexity ~O(lg(lg(N))) I think),
 		// I have to cache the powers_of_k that are used in dubious_min/max_nodes.
@@ -53,24 +53,31 @@ namespace OrderedMinHeap
 	}
 
 	template <typename Type>
-	void sort_min_siblings(Type *data, long before_first, long after_last, long tuple_size, long root, std::vector<bool> &trusty_matrix, std::vector<char> &depth_matrix, std::vector<std::vector<long>> &next_dubious_min_nodes, std::vector<std::vector<long>> &next_dubious_max_nodes, long min_right_line_of_implicit_trust, long max_left_line_of_implicit_trust)
+	void sort_min_siblings(Type *data, long before_first, long after_last, long tuple_size, long root, std::vector<bool> &trusty_matrix, std::vector<char> &depth_matrix, std::vector<std::vector<long>> &next_dubious_min_nodes, long min_right_line_of_implicit_trust)
 	{
 		long left_child = MinHeap::left_child(root, before_first, tuple_size);
 		long right_child = MinHeap::right_child(root, before_first, tuple_size);
-		if (right_child < after_last && data[left_child] > data[right_child])
+		// Start - Selection Sort
+		for (long child = left_child; child < right_child; child += 1)
 		{
-			// It's tempting to use cycle sort here, but I will not (at least for now)
-			// Interestingly, I think the best sort here is Selection Sort.
-			// If you find an item that isn't already in its correct position, then the swapping item's order must have increased.
-			// Additionally, because sorted lists are removed (the O(k) best case is less important)
-			// Selection Sort has a time complexity very similar to Insertion Sort in the worst case,
-			// it avoids some branch prediction problems,
-			// it reduces the number of swaps,
-			// and we are dealing with a small number of elements (O(k) e.g. 5 elements).
-			std::swap(data[left_child], data[right_child]);
-			verify_min_stability(data, before_first, after_last, right_child, trusty_matrix, depth_matrix, next_dubious_min_nodes, next_dubious_max_nodes, min_right_line_of_implicit_trust, max_left_line_of_implicit_trust);
-			verify_max_stability(data, before_first, after_last, left_child, trusty_matrix, depth_matrix, next_dubious_min_nodes, next_dubious_max_nodes, min_right_line_of_implicit_trust, max_left_line_of_implicit_trust);
+			long minimum = child;
+			for (long min_selector = child + 1; min_selector <= right_child; min_selector += 1)
+			{
+				minimum = (data[minimum] < data[min_selector] ? minimum : min_selector);
+			}
+			if (minimum != child)
+			{
+				if (minimum < min_right_line_of_implicit_trust && trusty_matrix[minimum] == true)
+				{
+					// TODO: VERIFY: something feels off
+					// You can no longer trust the node whose value increased (since it may no longer be a heap).
+					trusty_matrix[minimum] = false;
+					next_dubious_min_nodes[depth_matrix[minimum - before_first]].push_back(minimum);
+				}
+				std::swap(data[child], data[minimum]);
+			}
 		}
+		// End - Selection Sort
 	}
 
 	template <typename Type>
@@ -122,7 +129,7 @@ namespace OrderedMinHeap
 					while (min_iterator > 0)
 					{
 						min_iterator -= 1;
-						sort_min_siblings(data, before_first, after_last, dubious_min_siblings[depth][min_iterator], trusty_matrix, depth_matrix, dubious_min_nodes, dubious_max_nodes, min_right_line_of_implicit_trust, max_left_line_of_implicit_trust);
+						sort_min_siblings(data, before_first, after_last, tuple_size, dubious_min_siblings[depth][min_iterator], trusty_matrix, depth_matrix, dubious_min_nodes, min_right_line_of_implicit_trust);
 					}
 					dubious_min_siblings[depth].clear();
 				}
